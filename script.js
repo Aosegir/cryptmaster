b16a207322e140920f4aa5f3307ede7761c5fe54
Aosegir <128326345+Aosegir@users.noreply.github.com>
2024-05-19 09:45:37 -0700

    Updated usability: Code now works with proper functionality. No issues found.

--- script.js
+++ script.js
@@ -5,16 +5,28 @@ const SYN_LIST = document.getElementById('syn-list');
 const MAZ_LIST = document.getElementById('maz-list');
 const NIX_LIST = document.getElementById('nix-list');
 
+function findMatchingLetter(character, matchingLetter) {
+    let list = character.children;
+    for(let i = 0; i < list.length; i++) {
+        if(list[i].classList.contains(matchingLetter)) list[i].disabled = true;
+    };
+}
+
 letterList.forEach((list) => {
-    for(i = 0; i < 26; i++) {
+    for(let i = 0; i < 26; i++) {
         let button = document.createElement('button');
         let currentAlphabet = String.fromCharCode(65 + i);
         button.classList.add(`${currentAlphabet}`);
         button.textContent = currentAlphabet;
         // TEMP - FOR TESTING ONLY
-        button.addEventListener('click', () => {
-            button.disabled = true;
-        });
+        if(list.id == 'master-list') {
+            button.addEventListener('click', () => {
+                findMatchingLetter(JORO_LIST, currentAlphabet);
+                findMatchingLetter(SYN_LIST, currentAlphabet);
+                findMatchingLetter(MAZ_LIST, currentAlphabet);
+                findMatchingLetter(NIX_LIST, currentAlphabet);
+            });
+        };
         list.appendChild(button);
     }
 });
@@ -29,3 +41,10 @@ resetButton.forEach((button) => {
         };
     });
 });
+
+/*
+    Make master-list below four characters
+    FIll master-list like character-lists
+    On button click, loop through the other four character lists and disable
+        the appropriate button
+*/
